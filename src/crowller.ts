6da2e0d6a68da06268a026319d4e5059f778b6e6
sunzhaoxiang <sunzhaoxiang@baidu.com>
2022-10-09 13:04:57 +0800

    使用cheerio进行数据提取

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,12 +1,37 @@
 import superagent from 'superagent';
+import cheerio from 'cheerio';
+
+interface Course{
+    title: string,
+    count: number
+}
 
 class Crowller {
     private key = 'x3b174jsx';
     private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.key}`;
-    public rowHtml = '';
+
+    getCourseInfo(html: string) {
+        const $ = cheerio.load(html);
+        const courseItems = $('.course-item');
+        const courseInfo: Course[]= [];
+        courseItems.map((index, element) => {
+            const descs = $(element).find('.course-desc');
+            const title = descs.eq(0).text();
+            const count = parseInt(descs.eq(1).text().split('：')[1], 10);
+            courseInfo.push({
+                title,
+                count
+            });
+        });
+        const result = {
+            time: new Date().getTime(),
+            data: courseInfo
+        };
+    }
+
     async getRowHtml() {
         const data = await superagent.get(this.url);
-        this.rowHtml = data.text;
+        this.getCourseInfo(data.text);
     }
 
     constructor() {
@@ -16,4 +41,3 @@ class Crowller {
 }
 
 const crowller = new Crowller();
-console.log('fanfa===:', crowller);
